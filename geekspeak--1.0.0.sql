34c4d1493113d525f8443ecace906ab4a7aadfe3
Miles Elam <miles@geekspeak.org>
2017-02-09 09:36:42 -0800

    Integrated newsfeeds and content utilities

--- geekspeak--1.0.0.sql
+++ geekspeak--1.0.0.sql
@@ -3,8 +3,7 @@
 -- Miles Elam <miles@geekspeak.org>
 --
 -- Depends on audit
---            content_utils
---            newsfeeds
+--            plpgsql
 -- ---------------------------------------------------------------------------
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
@@ -202,6 +201,73 @@ CREATE INDEX episodes_published_idx ON episodes USING btree (published DESC NULL
 
 CREATE INDEX sessions_expires_for_reset_idx ON sessions USING btree (expires DESC NULLS LAST, for_reset NULLS FIRST);
 
+CREATE TABLE headlines (
+    id serial NOT NULL PRIMARY KEY,
+    source character varying(100) NOT NULL,
+    https boolean DEFAULT false NOT NULL,
+    url text NOT NULL UNIQUE,
+    title character varying(128) NOT NULL,
+    description text,
+    metadata jsonb NOT NULL,
+    discussion text,
+    labels character varying(100)[],
+    added timestamp without time zone DEFAULT now() NOT NULL,
+    fts tsvector,
+    archived timestamp without time zone,
+    teaser_image text,
+    content text,
+    summary text,
+    favicon text
+);
+
+COMMENT ON TABLE headlines IS
+'Raw article information. Should be purged if deemed sufficiently old and not incorporated into an'
+|| ' episode.';
+
+COMMENT ON COLUMN headlines.source IS
+'e.g., New York Times, Wired, BBC';
+
+COMMENT ON COLUMN headlines.url IS
+'URL without a protocol so that an https:// link is not considered distinct from http://.';
+
+COMMENT ON COLUMN headlines.title IS
+'Headline title.';
+
+COMMENT ON COLUMN headlines.description IS
+'Headline description; a summary of the content.';
+
+COMMENT ON COLUMN headlines.discussion IS
+'Link to conversation about the topic if anyone needs some perspective.';
+
+COMMENT ON COLUMN headlines.fts IS
+'Full Text Search vector';
+
+COMMENT ON COLUMN headlines.archived IS
+'If a link goes dead, reference the last archive.org grab.';
+
+COMMENT ON COLUMN headlines.metadata IS
+'Raw headline information. Dumping ground for any extra info that doesn''t need direct reference'
+|| ' from queries.';
+
+COMMENT ON COLUMN headlines.https IS
+'Whether a secure URL is available';
+
+COMMENT ON COLUMN headlines.teaser_image IS
+'Article-supplied thumbnail image';
+
+COMMENT ON COLUMN headlines.content IS
+'Page content; used for search.';
+
+COMMENT ON COLUMN headlines.summary IS
+'Not used currently. Placeholder for Chandler''s article summary algorithm.';
+
+COMMENT ON COLUMN headlines.favicon IS
+'Used for headline display to help differentiate sources easily.';
+
+CREATE INDEX added_idx ON headlines USING btree (added DESC NULLS LAST);
+
+CREATE INDEX fts_idx ON headlines USING gin (fts);
+
 CREATE TABLE bit_templates (
     id smallserial NOT NULL PRIMARY KEY,
     title character varying(126) NOT NULL,
@@ -345,6 +411,17 @@ LANGUAGE sql STABLE STRICT LEAKPROOF AS $$
     FROM gs.bits(ordered_bits) AS bits;
 $$;
 
+CREATE FUNCTION clean_query(query text) RETURNS text
+LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
+  WITH raw_query AS
+    (SELECT regexp_matches(replace(replace(replace(query, ':title', ':A'), ':description', ':B'), ':content', ':D'), '(\(?)\s*(''[^'']+''|"[^"]+"|[-''a-z0-9]+)\s*(\)?)(:\w)?\s*([&|]?)\s*', 'g') AS tokens)
+  SELECT rtrim(string_agg(concat(tokens[1], tokens[2], tokens[3], coalesce(nullif(tokens[4], ''), '&')),''),'&')
+    FROM raw_query;
+$$;
+
+COMMENT ON FUNCTION clean_query(query text) IS
+'Convert the user search query into something the full text search query engine can handle.';
+
 CREATE FUNCTION confirm(nonce uuid, plain_password character varying, ip inet)
     RETURNS TABLE(person jsonb, nonce uuid)
 LANGUAGE sql STRICT AS $$
@@ -431,6 +508,82 @@ LANGUAGE plpgsql AS $$
   RETURN NEW;
   END;
 $$;
+
+CREATE FUNCTION fts() RETURNS trigger
+LANGUAGE plpgsql AS $$
+  BEGIN
+  NEW.fts :=
+    setweight(to_tsvector((new.title)::text), 'A') ||
+    setweight(to_tsvector(coalesce((new.description)::text, '')), 'B') ||
+    setweight(to_tsvector((new.source)::text), 'C') ||
+    setweight(to_tsvector(coalesce(new.content, '')), 'D');
+  return NEW;
+  END
+$$;
+
+COMMENT ON FUNCTION fts() IS
+'Make sure the full text search (FTS) vector is updated for the search index whenever a change is'
+|| ' made to the headline.';
+
+CREATE FUNCTION hack_favicon() RETURNS void
+LANGUAGE sql LEAKPROOF AS $$
+  UPDATE headlines
+    SET favicon = array_to_string(regexp_matches(reify_url(https, url)::text,
+        '^https?://[^/]+/')::text[], ''::text) || 'favicon.ico'
+    WHERE favicon IS NULL OR favicon = 'favicon.ico';
+$$;
+
+COMMENT ON FUNCTION hack_favicon() IS
+'Convert relative favicon URLs to fully qualified URLs.';
+
+CREATE FUNCTION headlines(since interval DEFAULT '7 days'::interval,
+                          querytext text DEFAULT ''::text,
+                          min_rank real DEFAULT 0.1,
+                          lim integer DEFAULT 2000,
+                          oset integer DEFAULT 0)
+            RETURNS TABLE(id integer, rank real, added date, type character varying,
+                          source character varying, title character varying, url text,
+                          description text, discussion text, locale character varying,
+                          teaser_image text, favicon text, tags character varying[])
+LANGUAGE sql STABLE STRICT LEAKPROOF AS $$
+  WITH ts AS (SELECT length(querytext) > 0 AS has_query,
+                     gs.to_tsquery('english', querytext) AS query, now() - since AS cutoff)
+  (SELECT id, ts_rank(fts, ts.query) * rank_modifier(added) as rank, added::date,
+          metadata->>'type', source, title, reify_url(https, url), description, discussion,
+          metadata->>'locale', teaser_image, favicon, labels
+     FROM headlines h, ts
+     WHERE ts.has_query = true AND added > cutoff AND fts @@ ts.query
+           AND ts_rank(fts, ts.query) * rank_modifier(added) >= min_rank
+     ORDER BY rank DESC, id DESC
+     LIMIT lim
+     OFFSET oset)
+  UNION ALL
+  (SELECT id, 10.0 AS rank, added::date, metadata->>'type', source,
+          title, reify_url(https, url), description, discussion, metadata->>'locale',
+          teaser_image, favicon, labels
+     FROM headlines h, ts
+     WHERE ts.has_query = false AND added > cutoff
+     ORDER BY id DESC
+     LIMIT lim
+     OFFSET oset)
+  ORDER BY rank DESC, id DESC;
+$$;
+
+COMMENT ON FUNCTION headlines(since interval, querytext text, min_rank real, lim integer,
+                              oset integer) IS
+'Browse and search incoming news headlines.';
+
+CREATE FUNCTION headlines_as_json(since interval DEFAULT '7 days'::interval,
+                                  query text DEFAULT ''::text, min_rank real DEFAULT 1.0,
+                                  lim integer DEFAULT 2000, oset integer DEFAULT 0) RETURNS jsonb
+LANGUAGE sql STABLE STRICT LEAKPROOF AS $$
+  SELECT coalesce(jsonb_agg(headlines), '[]'::jsonb)
+    FROM headlines(since, query, min_rank, lim, oset) as headlines;
+$$;
+
+COMMENT ON FUNCTION headlines_as_json(since interval, query text, min_rank real, lim integer,
+                                      oset integer) IS
+'Browse and search incoming news headlines, returning them as JSON.';
 
 CREATE FUNCTION http(ts timestamp without time zone) RETURNS text
 LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
@@ -556,6 +709,30 @@ COMMENT ON FUNCTION person(nonce uuid, ip inet) IS
 'Get the person info as json, sanitized for security. This also updates the session info to extend
  the session expiration and record the current ip address.';
 
+CREATE FUNCTION rank_modifier(age interval) RETURNS real
+LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
+  -- 60 seconds * 60 minutes * 24 hours * 7 days
+  SELECT (1 / ceil(extract(epoch from age) / 604800))::real;
+$$;
+
+COMMENT ON FUNCTION rank_modifier(age interval) IS
+'Older stuff should be less likely to show up in search results.';
+
+CREATE FUNCTION rank_modifier(moment timestamp without time zone DEFAULT now()) RETURNS real
+LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
+  SELECT rank_modifier(now() - moment);
+$$;
+
+COMMENT ON FUNCTION rank_modifier(moment timestamp without time zone) IS
+'Older stuff should be less likely to show up in search results.';
+
+CREATE FUNCTION reify_url(https boolean, url character varying) RETURNS character varying
+LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
+  SELECT CASE WHEN https THEN 'https:' ELSE 'http:' END || url;
+$$;
+
+COMMENT ON FUNCTION reify_url(https boolean, url character varying) IS 'Add protocol to a URL. This is necessary because we want http and https to be considered equivalent when determining unique headlines.';
+
 CREATE FUNCTION record(id episode_num) RETURNS TABLE(season smallint, episode smallint)
 LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
   SELECT (id >> 9)::smallint, (id & 511)::smallint;
@@ -597,6 +774,14 @@ COMMENT ON FUNCTION register(email character varying, ip inet, user_agent charac
  emailed and used to call gs.confirm(nonce, password, ip_address) to enable access.
 
 Note: the person must be given the correct ACLs to perform most actions.';
+
+CREATE FUNCTION source(source character varying, url character varying) RETURNS character varying
+LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
+  SELECT coalesce(source, regexp_replace(url, '^https?://(?:www\.)?([^/]+)/.+$', '\1'));
+$$;
+
+COMMENT ON FUNCTION source(source character varying, url character varying) IS
+'Provide a headline''s source either by explicit value or by using the domain name.';
 
 CREATE FUNCTION text(id episode_num) RETURNS text
 LANGUAGE sql IMMUTABLE STRICT LEAKPROOF AS $$
