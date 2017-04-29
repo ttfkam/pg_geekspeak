2ca1730129cf5fd868ae700b4ca939298c3ba4fb
Miles Elam <miles@geekspeak.org>
2017-04-29 11:12:41 -0700

    Test updates and cleanup

--- sql/geekspeak_test.sql
+++ sql/geekspeak_test.sql
@@ -35,16 +35,14 @@ SELECT c.person, c.nonce IS NOT NULL
   FROM cte, confirm(cte.nonce, 'TestPassword1$', '10.11.12.13') AS c;
 
 --
--- Verify active logins
+-- Verify only active logins
 --
 
-SELECT count(*)
-  FROM sessions
-  WHERE for_reset = true OR expires < now();
+-- 0 pending registrations or expired sessions
+SELECT count(*) FROM sessions WHERE for_reset = true OR expires < now();
 
-SELECT count(*)
-  FROM sessions
-  WHERE for_reset = false AND expires > now();
+-- 1 active session
+SELECT count(*) FROM sessions WHERE for_reset = false AND expires > now();
 
 --
 -- Bad login: wrong password
@@ -62,20 +60,18 @@ SELECT login('test@example.com', 'TestPassword1$', '10.1.2.3', 'Test Agent') IS 
 -- Logout
 --
 
-SELECT logout(nonce, '10.1.2.3')
-  FROM sessions WHERE for_reset = false AND ips[1] = '10.1.2.3';
+SELECT logout(nonce, '10.2.4.6')
+  FROM sessions WHERE for_reset = false;
 
 --
 -- Verify active logins
 --
 
-SELECT count(*)
-  FROM sessions
-  WHERE for_reset = true OR expires < now();
+-- 1 expired session
+SELECT count(*) FROM sessions WHERE for_reset = true OR expires < now();
 
-SELECT count(*)
-  FROM sessions
-  WHERE for_reset = false AND expires > now();
+-- 0 active sessions
+SELECT count(*) FROM sessions WHERE for_reset = false AND expires > now();
 
 --
 -- Add news items
@@ -113,7 +109,7 @@ SELECT add_headline_bit(
 -- Utility functions used by clients
 --
 
-SELECT http('2016-04-01 16:20'::timestamp);
+SELECT http('2016-04-01 16:20'::timestamptz AT TIME ZONE 'Pacific');
 
 SELECT http('Fri, 01 Apr 2016 16:20:00');
 
@@ -125,13 +121,15 @@ SELECT add_ip('{"127.0.0.1", "127.0.0.2", "127.0.0.3"}'::inet[], '127.0.0.1'::in
 
 SELECT add_ip('{"127.0.0.1", "127.0.0.2", "127.0.0.3"}'::inet[], '127.0.0.4'::inet);
 
-SELECT episode_num('2016-04-01'::date);
-
 SELECT episode_num(16, 12);
+
+SELECT text(episode_num(16, 12));
+
+SELECT text(8204::episode_num);
 
 SELECT mime_type('png'), mime_type('jpg'), mime_type('svgz'), mime_type('mp3');
 
-SELECT record(8204::episode_num);
+SELECT * FROM record(8204::episode_num);
 
 SELECT reify_url(true, '//www.example.com/test/https');
 
@@ -142,8 +140,6 @@ SELECT source('New York Times', 'https://www.nytimes.com/articles/testing');
 SELECT source(NULL::varchar, 'https://www.nytimes.com/articles/testing');
 
 SELECT source(NULL::varchar, NULL::varchar);
-
-SELECT episode_as_text(8204::episode_num);
 
 SELECT validate_password('password'), validate_password('PASSWORD'), validate_password('pAsSwOrD'),
        validate_password('passwd'), validate_password('Password9'), validate_password('pA$$w0rd');
